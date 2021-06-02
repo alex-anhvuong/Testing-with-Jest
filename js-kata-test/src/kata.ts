8977cc0fcca94b240a98e17b6c7278a99349c461
Alex Vuong <vavuong@deakin.edu.au>
2021-06-02 20:03:22 +1000

    Check for negative numbers

--- js-kata-test/src/kata.ts
+++ js-kata-test/src/kata.ts
@@ -1,7 +1,30 @@
 function Add(numbers: string) {
     if (numbers == "") return 0;
-    let nums: string[] = numbers.split(/,|\n/);
+    let delim = ',';
+
+    //  Check if a different delimiters is used
+    let newDelim: number = numbers.indexOf("//")
+    if (newDelim != -1) {
+        delim = numbers[newDelim + 2];
+        numbers = numbers.substring(numbers.indexOf("\n") + 1);
+    }
+    let regexString = delim + '|\n';
+    const regex = new RegExp(regexString);
+
+    let nums: string[] = numbers.split(regex);
     let parsedNums = nums.map(str => parseInt(str));
+
+    //  Check for negative numbers
+    let negs: number[] = [];
+    parsedNums.map((num) => {
+        if (num < 0) negs.push(num);
+    });
+
+    if (negs.length > 0) {
+        let msg: string = negs.join(",");
+        throw new Error("Negatives not allowed: " + msg);
+    }
+
     let sum = parsedNums.reduce((a, b) => a + b);
     return sum;
 }
--- js-kata-test/test/kata.test.js
+++ js-kata-test/test/kata.test.js
@@ -15,3 +15,13 @@ test("Check Add numbers with new lines between numbers", () => {
     expect(Add("1\n2")).toBe(3);
     expect(Add("1,2,3\n4,5,6,7\n8,9,10")).toBe(55);
 });
+
+test("Check Add numbers with different delimiters", () => {
+    expect(Add("//k\n1k2")).toBe(3);
+    expect(Add("//`\n1`2`3`4`5\n6`7`8\n9\n10")).toBe(55);
+});
+
+test("Throw execeptions with negative numbers", () => {
+    expect(() => Add("1,2,-1,0,-2,-3")).toThrow(new RegExp("-1,-2,-3"));
+    expect(() => Add("//;\n1;-2;-1;0")).toThrow(new RegExp("-1"));
+})
